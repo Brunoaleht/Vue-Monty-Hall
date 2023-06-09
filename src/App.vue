e5717d40e5e3bd809a77afe9cdba957836206c53
Bruno Alehtxandre <brunoaleh1@gmail.com>
2023-06-09 17:45:16 -0300

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Problema de MontyHall</h1>
+    <div class="form">
+      <div v-if="!started">
+        <label for="portsAmount">How many doors? </label>
+        <input
+          type="text"
+          id="portsAmount"
+          size="3"
+          v-model.number="portsAmount"
+        />
+      </div>
+      <div v-if="!started">
+        <label for="giftedPort">What is the chosen port? </label>
+        <input
+          type="text"
+          id="giftedPort"
+          size="3"
+          v-model.number="giftedPort"
+        />
+      </div>
+      <button v-if="!started" @click="started = true">Start</button>
+      <button v-if="started" @click="started = false">Re-Start</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <DoorH
+          :hasGift="i === giftedPort"
+          :number="i"
+          @portSelected="portSelected"
+          :selected="selectedPort == i"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DoorH from "./components/DoorH.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { DoorH },
+  data: function () {
+    return {
+      started: false,
+      portsAmount: 5,
+      giftedPort: null,
+      selectedPort: null,
+    };
+  },
+  methods: {
+    portSelected(selectedPort) {
+      this.selectedPort = selectedPort;
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: "Montserrat", sans-serif;
+  box-sizing: border-box;
+}
+body {
+  color: #fff;
+  background: linear-gradient(to right, #08aeea 0%, #2af598 100%);
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#app h1 {
+  border: solid 1px #000;
+  background-color: #0006;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+.form,
+.form input,
+.form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/GiftBox.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="gift">
+    <div class="giftTie1"></div>
+    <div class="giftTie2"></div>
+    <div class="giftLid"></div>
+    <div class="giftBox"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiftBox",
+};
+</script>
+
+<style>
+.gift {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 10px;
+}
+.giftLid {
+  width: 100px;
+  height: 20px;
+  background-color: rgb(99, 99, 197);
+}
+.giftBox {
+  width: 90px;
+  height: 60px;
+  background-color: rgb(54, 54, 112);
+}
+.giftTie1 {
+  position: absolute;
+  width: 15px;
+  height: 80px;
+  background-color: red;
+}
+.giftTie2 {
+  position: absolute;
+  top: 35px;
+  width: 90px;
+  height: 15px;
+  background-color: red;
+}
+</style>
